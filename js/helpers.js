ada4cb821971a645fd2f299415f80277401075f7
unknown <arbel8@gmail.com>
2021-10-17 12:14:12 +0300

    test upload

---
+++ js/helpers.js
@@ -0,0 +1,35 @@
+export function query(selector, scope) {
+	if (scope === null) throw 'query scope cannot be null';
+	return (scope || document).querySelector(selector);
+}
+
+export function listen(target, type, callback, capture) {
+	target.addEventListener(type, callback, !!capture);
+}
+
+/* this function gets a HTML DOM element and excecute a function when an event (click/keypress/ etc.) 
+* occur on the specific element.
+* @param target HTML DOM element
+* @param selector class/id/element etc.
+* @param handler event to listen to
+* @param capture a function to execute when handler 
+*/
+export function delegateEvent(target, selector, type, handler, capture) {
+	const dispatchEvent = event => {
+		const targetElement = event.target;
+		const potentialElements = target.querySelectorAll(selector);
+		let i = potentialElements.length;
+
+		while (i--) {
+			if (potentialElements[i] === targetElement) {
+				handler.call(targetElement, event);
+				break;
+			}
+		}
+	};
+
+	listen(target, type, dispatchEvent, !!capture);
+}
+
+export const escapeForHTML = s => s.replace(/[&<]/g, c => c === '&' ? '&amp;' : '&lt;');
+
